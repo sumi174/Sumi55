f8b5bb2415c9d962c9d36dfe5e2a6e1482a38462
sumi174 <sumiakter1552@gmail.com>
2024-11-24 22:59:14 +0600

    Add files via upload

---
+++ online class/Product.java
@@ -0,0 +1,16 @@
+package javacode;
+
+public class Product {
+	
+		public static void main(String[] args) {
+			System.out.println("Id : 101");
+			System.out.println("Title : iphone15");
+			System.out.println("price : 1895 eurus");
+			System.out.println("Description : Perfect Product With best image quality.");
+			System.out.println("Category : phone");
+
+
+		}
+	}
+
+
